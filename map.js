7bf5b32f4271f42009f9a5d82be262e785a5a153
Robb Winkle <robb.winkle@icfnext.com>
2020-03-21 08:53:54 -0400

    Change marker to circle markers like the R solution. Both are using leaflet so I just borrowed the configurations

--- map.js
+++ map.js
@@ -4,9 +4,16 @@ let App = window.App || {};
 
 App.map = function () {
     let geocoder = new L.Control.Geocoder.nominatim();
+    let radiussize = 8;
+    let colorMap = {};
+    let pal = ["yellow", "green", "red"];
+    let statusTypes = ["Limited Hours", "Regular Hours", "Temporarily Closed"].forEach((element, i) => {
+        colorMap[element] = pal[i];
+    });
+    console.log(colorMap);
 
     function init() {
-        let mymap = L.map('mapid').setView([39.983334, -82.983330], 11);
+        let mymap = L.map('mapid').setView([39.983334, -82.983330], 10);
         console.log(mymap);
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
@@ -23,18 +30,33 @@ App.map = function () {
                 let r = results[0];
                 console.log(r);
                 location.position = r.center;
-                addMarker(mymap, location);
+                addCircle(mymap, location);
             });
         });
     }
 
+    function addCircle(mymap, info) {
+        let circle = L.circleMarker(info.position, {
+            fill: true,
+            color: colorMap[info.data.status],
+            fillColor: colorMap[info.data.status],
+            fillOpacity: 0.66,
+            stroke: true,
+            weight: 1
+        }).addTo(mymap);
+        circle.bindPopup("Name:" + info.data.name + "<br>"+
+        "Address:" + info.data.street + "<br>" +
+        "Status:" + info.data.status + "<br>" +
+        "Phone #:<a href=\"tel:" + "(614) 555-5555" + "\">" + "(614) 555-5555" + "</a>" + "<br>");
+    }
+
     function addMarker(mymap, info) {
         let marker = L.marker(info.position).addTo(mymap);
-        marker.bindPopup("<b>" + info.data.name + "</b><br>" + info.data.address);
+        marker.bindPopup("<b>" + info.data.name + "</b><br>" + info.data.street);
     }
 
     function buildQuery(locationData) {
-        return locationData.data.address + ", " 
+        return locationData.data.street + ", " 
             + locationData.data.city + ", " 
             + locationData.data.state + " "
             + locationData.data.zip;
@@ -45,22 +67,36 @@ App.map = function () {
                 "position":[39.983334, -82.983330],
                 "data": {
                     "name":"Barroluco Argentine Comfort Food",
-                    "address":"47 N Pearl St",
+                    "street":"47 N Pearl St",
                     "city":"Columbus",
                     "state":"OH",
-                    "zip":"43215"
+                    "zip":"43215",
+                    "status":"Regular Hours"
                 }
             },
             {
                 "position":[39.983334, -82.983330],
                 "data": {
                     "name":"Jeni's Splendid Ice Creams - North Market",
-                    "address":"59 Spruce St.",
+                    "street":"59 Spruce St.",
                     "city":"Columbus",
                     "state":"OH",
-                    "zip":"43215"
+                    "zip":"43215",
+                    "status":"Limited Hours"
+                }
+            },
+            {
+                "position":[39.983334, -82.983330],
+                "data": {
+                    "name":"Harvest Pizza - Dublin",
+                    "street":"45 N. High St.",
+                    "city":"Dublin",
+                    "state":"OH",
+                    "zip":"43017",
+                    "status":"Temporarily Closed"
                 }
             }
+
         ];
     }
     init();
